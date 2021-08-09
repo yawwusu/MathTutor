34c1c8128133f2f8439f94e10afb53d76dcabbe2
yawwusu <yawwusugh@gmail.com>
2021-08-09 02:57:33 +0200

    NavBar and Footer basic styling

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,35 @@
+import { useAuth } from "../lib/auth";
+import { UserIcon, UserCircleIcon } from "@heroicons/react/solid";
+// import { UserCircleIcon } from "@heroicons/react/solid";
+
 function NavBar() {
+  const auth = useAuth();
+  const handleAuth = () =>
+    !auth.user ? auth.signinWithGoogle() : auth.signout();
+
   return (
-    <nav className="flex items-center justify-center w-full h-12 border-b">
-      <p>Nav Bar</p>
+    <nav className="flex items-center justify-between p-2 w-full h-12 border-b">
+      <img src="/logo.svg" alt="Math Tutor Logo" className="h-10 ml-2" />
+      <div
+        onClick={handleAuth}
+        className="group flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-black"
+      >
+        {!auth.user ? (
+          <>
+            <UserIcon className="h-8 pt-2 group-hover:animate-bounce" />
+            <p className="tracking-widest opacity-0 group-hover:opacity-100">
+              Log In
+            </p>
+          </>
+        ) : (
+          <>
+            <UserCircleIcon className="h-10 pt-2 group-hover:animate-bounce" />
+            <p className="tracking-widest opacity-0 group-hover:opacity-100">
+              Log Out
+            </p>
+          </>
+        )}
+      </div>
     </nav>
   );
 }
